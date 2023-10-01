218fb89a37e534aec37f5c93510b940163434cff
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-01 23:26:46 +0700

    add utils

---
+++ src/utils/song.js
@@ -0,0 +1,13 @@
+const mapDbToModel = ({id, title, year, genre, performer, duration, albumId, inserted_at, updated_at}) => ({
+    id,
+    title,
+    year,
+    genre,
+    performer,
+    duration,
+    albumId,
+    insertedAt: inserted_at,
+    updatedAt: updated_at,
+    });
+
+module.exports = { mapDbToModel };
