ec8321ac849a7d249c2daf5815c1cd2d834967d8
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-04 09:48:51 +0700

    fix on quotations

--- migrations/1696179286483_create-table-albums.js
+++ migrations/1696179286483_create-table-albums.js
@@ -1,31 +1,32 @@
 exports.up = (pgm) => {
-    pgm.createTable('albums', {
-        id: {
-            type: 'VARCHAR(80)',
-            notNull: true,
-            primaryKey: true,
-        },
-        name: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        year: {
-            type: 'INT',
-            notNull: true,
-        },
-        inserted_at: {
-            type: 'TIMESTAMP',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-        updated_at: {
-            type: 'TIMESTAMP',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
+    pgm.createTable("albums", {
+      id: {
+        type: "VARCHAR(80)",
+        notNull: true,
+        primaryKey: true,
+      },
+      name: {
+        type: "VARCHAR(50)",
+        notNull: true,
+      },
+      year: {
+        type: "INT",
+        notNull: true,
+      },
+      inserted_at: {
+        type: "TIMESTAMP",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
+      updated_at: {
+        type: "TIMESTAMP",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
     });
-};
-
-exports.down = (pgm) => {
-    pgm.dropTable('albums');
-};
+  };
+  
+  exports.down = (pgm) => {
+    pgm.dropTable("albums");
+  };
+  
--- migrations/1696179490255_create-table-songs.js
+++ migrations/1696179490255_create-table-songs.js
@@ -1,52 +1,53 @@
 exports.up = (pgm) => {
-    pgm.createTable('songs', {
-        id: {
-            type: 'VARCHAR(80)',
-            primaryKey: true,
-        },
-        title: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        year: {
-            type: 'INT',
-            notNull: true,
-        },
-        genre: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        performer: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        duration: {
-            type: 'INT',
-            notNull: true,
-        },
-        albumId: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        created_at: {
-            type: 'TIMESTAMP',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-        updated_at: {
-            type: 'TIMESTAMP',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
+    pgm.createTable("songs", {
+      id: {
+        type: "VARCHAR(80)",
+        primaryKey: true,
+      },
+      title: {
+        type: "VARCHAR(50)",
+        notNull: true,
+      },
+      year: {
+        type: "INTEGER",
+        notNull: true,
+      },
+      genre: {
+        type: "VARCHAR(50)",
+        notNull: true,
+      },
+      performer: {
+        type: "VARCHAR(50)",
+        notNull: true,
+      },
+      duration: {
+        type: "INT",
+        notNull: false
+      },
+      albumId: {
+        type: "VARCHAR(50)",
+        notNull: false,
+      },
+      created_at: {
+        type: "TIMESTAMP",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
+      updated_at: {
+        type: "TIMESTAMP",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
     });
-};
-
-pgm.addConstraint(
-    'songs',
-    'fk_songs.albums_albumId.albums.id',
-    'FOREIGN KEY(albumId) REFERENCES albums(id) ON DELETE CASCADE'
-)
-
-exports.down = (pgm) => {
-    pgm.dropTable('songs');
-};
+  
+    pgm.addConstraint(
+      "songs",
+      "fk_album.album_id_albums.id",
+      'FOREIGN KEY ("albumId") REFERENCES albums(id) ON DELETE CASCADE'
+    );
+  };
+  
+  exports.down = (pgm) => {
+    pgm.dropTable("songs");
+  };
+  
