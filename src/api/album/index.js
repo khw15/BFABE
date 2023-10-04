ef9061794be6f8686708921542e7bff9e388bc37
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-04 10:22:45 +0700

    add comments for clarity

--- src/api/album/index.js
+++ src/api/album/index.js
@@ -5,7 +5,10 @@ module.exports = {
   name: 'albums',
   version: '0.0.1',
   register: async (server, {service, validator}) => {
+    // Create an instance of the AlbumHandler class
     const albumHandler = new AlbumHandler(service, validator);
+
+    // Register the routes defined in the 'routes' module with the albumHandler
     server.route(routes(albumHandler));
   },
 };
